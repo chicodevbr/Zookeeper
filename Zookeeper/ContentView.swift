a2feff41eebae16986b7811d6f510136f3edfd4d
chicodevbr <chicodevbr@gmail.com>
2022-10-17 12:52:29 -0300

    add forEach and sections

--- Zookeeper/ContentView.swift
+++ Zookeeper/ContentView.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var animals = [
-        Animal(name: "Dog"),
-        Animal(name: "Cat"),
-        Animal(name: "Parrot")
-    ]
-    
     var body: some View {
         VStack {
-            List(animals) {
-                animal in Text(animal.name)
-            }
-            Button("Add") {
-                animals.append(Animal(name: "New Animal"))
+            List {
+                ForEach(animalGroups) { animalGroup in
+                    Section(header:
+                                Text(animalGroup.groupName)) {
+                        ForEach(animalGroup.animals) {
+                            animal in
+                            Text(animal.name)
+                        }
+                    }
+                    
+                }
             }
         }
         
@@ -32,6 +32,32 @@ struct Animal: Identifiable {
     var name: String
 }
 
+struct AnimalGroup: Identifiable {
+    var id = UUID()
+    var groupName: String
+    var animals: [Animal]
+}
+
+let animalGroups = [
+    AnimalGroup(groupName: "Pets", animals: [
+        Animal(name: "Dog"),
+        Animal(name: "Cat"),
+        Animal(name: "Parrot"),
+        Animal(name: "Mouse")
+]),
+    AnimalGroup(groupName: "Farm", animals: [
+        Animal(name: "Cow"),
+        Animal(name: "Horse"),
+        Animal(name: "Goat"),
+        Animal(name: "Sheep")
+]),
+    AnimalGroup(groupName: "Critters", animals: [
+        Animal(name: "Firefly"),
+        Animal(name: "Spider"),
+        Animal(name: "Ant"),
+        Animal(name: "Squirrel")
+])
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

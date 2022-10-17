a27d01355d3e194ce5794a8c5e8fd096e98f1cc5
chicodevbr <chicodevbr@gmail.com>
2022-10-17 11:08:04 -0300

    Initial Commit

---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by fjunior on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

a741d045aa73903cab9ac3909d23801b37045017
chicodevbr <chicodevbr@gmail.com>
2022-10-17 12:09:31 -0300

    initial project

--- Zookeeper/ContentView.swift
+++ Zookeeper/ContentView.swift
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var animals = [
+        Animal(name: "Dog"),
+        Animal(name: "Cat"),
+        Animal(name: "Parrot")
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List(animals) {
+                animal in Text(animal.name)
+            }
+            Button("Add") {
+                animals.append(Animal(name: "New Animal"))
+            }
         }
-        .padding()
+        
     }
 }
+
+struct Animal: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
